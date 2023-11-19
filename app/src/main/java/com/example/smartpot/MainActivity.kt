0dc01a99cf16aa395b3d2b6222275e1bf4514f60
EJ-pro <hsshss2522@naver.com>
2023-11-20 05:25:49 +0900

    바텀네비게이션 구현완료

---
+++ app/src/main/java/com/example/smartpot/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.smartpot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var BottomNV: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        BottomNV = findViewById(R.id.nav_view)
+        BottomNV.setOnNavigationItemSelectedListener { menuItem ->
+            bottomNavigate(menuItem.itemId)
+            true
+        }
+        BottomNV.selectedItemId = R.id.navigation_1
+    }
+    private fun bottomNavigate(id: Int) {
+        val tag = id.toString()
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        val currentFragment = fragmentManager.primaryNavigationFragment
+        currentFragment?.let { fragmentTransaction.hide(it) }
+
+        var fragment = fragmentManager.findFragmentByTag(tag)
+        if (fragment == null) {
+            fragment = when (id) {
+                R.id.navigation_1 -> Fragment_Main_page()
+                R.id.navigation_2 -> Fragment_Plant_Diary_page()
+                R.id.navigation_3 -> Fragment_Store_page()
+                else -> Fragment_My_page()
+            }
+            fragmentTransaction.add(R.id.content_layout, fragment, tag)
+        } else {
+            fragmentTransaction.show(fragment)
+        }
+
+        fragmentTransaction.setPrimaryNavigationFragment(fragment)
+        fragmentTransaction.setReorderingAllowed(true)
+        fragmentTransaction.commitNow()
+    }
+}
