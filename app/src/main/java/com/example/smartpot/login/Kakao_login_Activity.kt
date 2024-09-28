ee5bb6fcf935378f5b2994fdd0e19bad5764d0ed
SuperNoobChallenge <118657115+SuperNoobChallenge@users.noreply.github.com>
2024-09-28 20:34:49 +0900

    test

--- app/src/main/java/com/example/smartpot/login/Kakao_login_Activity.kt
+++ app/src/main/java/com/example/smartpot/login/Kakao_login_Activity.kt
@@ -22,6 +22,7 @@ class Kakao_login_Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         _binding = ActivityKakaoLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
